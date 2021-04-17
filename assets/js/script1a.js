e5c56b7cef47392b60bed15c0e4076a7f189a756
KieronOC <kieron@lighthouse12.com>
2021-04-17 23:49:09 +0100

    Added top card image and css to hide transparent image background.

--- assets/js/script1a.js
+++ assets/js/script1a.js
@@ -1,5 +1,27 @@
 document.addEventListener("DOMContentLoaded", function() {});
+// Displays the time : Learning source and come code from : https://www.codesdope.com/blog/article/how-to-create-a-digital-clock-using-javascript/
 
+function currentTime() {
+    var date = new Date();
+    var hour = date.getHours();
+    var min = date.getMinutes();
+    var sec = date.getSeconds();
+
+    hour = updateTime(hour);
+    min = updateTime(min);
+    sec = updateTime(sec);
+
+    function updateTime(k) {
+        if (k < 10) {
+            return "0" + k;
+        } else {
+            return k;
+        }
+    }
+    document.getElementById("clock").innerHTML = 'Current Time is : ' + hour + " : " + min + " : " + sec;
+    var t = setTimeout(function() { currentTime() }, 1000);
+}
+currentTime();
 
 // An array of photos to use as background images
 var pictures = [
@@ -36,6 +58,37 @@ var start = document.querySelector('#start');
 // Button to start timer and shuffle the cards on screen
 start.addEventListener('click', timedCount);
 start.addEventListener('click', shuffleArray);
+start.addEventListener('click', play);
+
+var c = 00;
+var t;
+var timer = document.getElementById("stopwatch");
+
+function timedCount() {
+
+    timer.innerHTML = 0;
+
+    function check(i) {
+        if (i < 10) {
+            i = "0" + i;
+        }
+        return i;
+    }
+    var k = c;
+    if (k > 59) {
+        var m = Math.floor(k / 60);
+        var s = k % 60;
+        var min = check(m);
+        var sec = check(s);
+        var l = min + 'm ' + ': ' + sec + 's';
+        timer.innerHTML = l;
+    } else {
+        k = '00m ' + ': ' + check(c) + 's';
+        timer.innerHTML = k;
+    }
+    c = c + 1;
+    t = setTimeout(timedCount, 1000);
+}
 
 
 // Gets the player to in effect refresh the page before pressing the Start button to start the timer and shuffle the cards
@@ -87,7 +140,28 @@ function shuffleArray() {
         faceUp.style.backgroundImage = `url(${pictures[i].img})`;
     }
 
+    return;
 
+}
+
+
+function play() {
+    var gameCards = document.getElementsByClassName('carD');
+    for (let game of gameCards) {
+
+        game.addEventListener('click', function(event) {
+
+            var choice = event.target;
+            choice.style.border = ('2px solid red')
+            if (choice === previousTarget) {
+                return;
+            }
+
+
+        })
+
+
+    }
 }
 
 
@@ -100,64 +174,10 @@ function shuffleArray() {
 
 
 
-// Displays the time : Learning source and come code from : https://www.codesdope.com/blog/article/how-to-create-a-digital-clock-using-javascript/
-
-function currentTime() {
-    var date = new Date();
-    var hour = date.getHours();
-    var min = date.getMinutes();
-    var sec = date.getSeconds();
-
-    hour = updateTime(hour);
-    min = updateTime(min);
-    sec = updateTime(sec);
-
-    function updateTime(k) {
-        if (k < 10) {
-            return "0" + k;
-        } else {
-            return k;
-        }
-    }
-    document.getElementById("clock").innerHTML = 'Current Time is : ' + hour + " : " + min + " : " + sec;
-    var t = setTimeout(function() { currentTime() }, 1000);
-}
-currentTime();
 
 
-// Timer code learnt from W3 Schools and my own interpretation from there.
-start.addEventListener('click', timedCount);
-start.addEventListener('click', shuffleArray);
 
 
-var c = 55;
-var t;
-var timer = document.getElementById("stopwatch");
-
-function timedCount() {
-    timer.innerHTML = 0;
-
-    function check(i) {
-        if (i < 10) {
-            i = "0" + i;
-        }
-        return i;
-    }
-    var k = c;
-    if (k > 59) {
-        var m = Math.floor(k / 60);
-        var s = k % 60;
-        var min = check(m);
-        var sec = check(s);
-        var l = min + 'm ' + ': ' + sec + 's';
-        timer.innerHTML = l;
-    } else {
-        k = '00m ' + ': ' + check(c) + 's';
-        timer.innerHTML = k;
-    }
-    c = c + 1;
-    t = setTimeout(timedCount, 1000);
-}
 
 
 // Put all the cards into an array 
